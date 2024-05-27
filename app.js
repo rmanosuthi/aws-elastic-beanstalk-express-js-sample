9ced583bf716d18e71f56140958a19608c5c49dc
Ratchaphon Manosuthi <themelo64@gmail.com>
2024-05-27 13:27:00 -0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-// a comment
+// a comment boo boo
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port);
